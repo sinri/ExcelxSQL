a2f358ada87ed2bd8e414ce34ed4303762e7b6ee
Everstray Jun Sinri Edogawa <e.joshua.s.e@gmail.com>
2017-10-27 15:19:34 +0800

    safe vendor autoload

--- autoload.php
+++ autoload.php
@@ -5,7 +5,8 @@
  * Date: 2017/10/1
  * Time: 21:32
  */
-
-require_once __DIR__ . '/vendor/autoload.php';
+if(file_exists(__DIR__ . '/vendor/autoload.php')){
+    require_once __DIR__ . '/vendor/autoload.php';
+}
 require_once __DIR__ . '/SQLite/LibSqlite3.php';
 require_once __DIR__ . '/ExcelAgent/ExcelSqliteAgent.php';
